f430436d008e1e2393c848cb2f516a7c5d84a065
DianMercado <mercadodiana38@gmail.com>
2022-10-31 13:48:44 -0600

    trainers/create

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,17 +1,22 @@
 @extends("layouts.app")
 @section('title', 'Trainers Create')
 @section('content')
-<form class="form-group" method="POST" action="/trainers" enctype="multipart/form-data">
-@csrf
 
+{!!Form::open (['route'=>'trainers.store', 'method'=>'POST','files'=>'true'])!!}
+<div class="container " >
 <div clas="form-group">
-    <label for="">Nombre: </label>
-    <input type="text"  name= "name" class="form-control">
-    <label for="">Apellido: </label>
-    <input type="text"  name= "Apellido" class="form-control">
-    <label for="">Avatar: </label>
-    <input type="file"  name= "Avatar" class="form-control">
+    {{Form::label('name','Nombre')}}
+    {{Form::text('name', null,['class'=>'form-control'])}}
+    {{Form::label('Apellido','Apellido')}}
+    {{Form::text('Apellido', null,['class'=>'form-control'])}}
 </div>
-<button type="submit" class="btn btn-primary">Guardar</button>
-</form>
+<div clas="form-group">
+    {{Form::label('Avatar','Avatar')}}
+    {{Form::file('Avatar')}}
+</div>
+
+{{Form::submit('Guardar',['class'=>'btn btn-primary'])}}
+{!!Form::close()!!}
+
 @endsection
+</div>
