2bf9debee9eb05c61ac2a3332abbb434b260a5c3
DianMercado <mercadodiana38@gmail.com>
2022-11-15 12:05:32 -0600

    Debug Bar

--- config/app.php
+++ config/app.php
@@ -163,6 +163,8 @@ return [
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
+
 
         /*
          * Package Service Providers...
@@ -231,6 +233,8 @@ return [
         'Form'=> Collective\Html\FormFacade::class,
         'Html'=> Collective\Html\HtmlFacade::class,
         'PDF' => Barryvdh\DomPDF\Facade::class,
+        'Debugbar'=> Barryvdh\Debugbar\Facade::class,
+        'Debug'=>env('APP_DEBUG', true),
 
     ],
 
